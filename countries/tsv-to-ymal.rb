d73b28dd7d317de0769fc62b96f5608437da70fc
gregology <greg@gho.st>
2013-05-22 14:25:55 -0400

    first commit

---
+++ countries/tsv-to-ymal.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require 'csv'
+require 'yaml'
+
+countries = Hash.new {|hash,key| hash[key] = nil }
+
+CSV.foreach("countries.tsv", { :col_sep => "\t" }) do | row |
+
+  alpha2 = row[0].nil? ? nil : row[0].strip
+  alpha3 = row[1].nil? ? nil : row[1].strip
+  numeric = row[2].nil? ? nil : row[2].strip
+  name = row[3].nil? ? nil : row[3].strip
+  altnames = row[4].nil? ? nil : row[4].split(',').map(&:lstrip)
+  adjectivals = row[5].nil? ? nil : row[5].split(',').map(&:lstrip)
+  demonyms = row[6].nil? ? nil : row[6].split(',').map(&:lstrip)
+
+  countries[alpha2] = {
+    :name => name,
+    :aplha3 => alpha3,
+    :numeric => numeric,
+    :altnames => altnames,
+    :adjectivals => adjectivals,
+    :demonyms => demonyms
+  } unless name == 'name'
+
+end
+
+puts countries.inspect
+
+open("countries.yml", 'w') {|f| YAML.dump(countries, f)}
+loaded = open("countries.yml") {|f| YAML.load(f) }
